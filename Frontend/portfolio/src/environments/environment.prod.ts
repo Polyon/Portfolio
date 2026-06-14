7db03c19fdb24daacfa425443ba4fe8995923367
Polyon <polyonmondal@gmail.com>
2026-06-14 17:09:47 +0530

    refactor: update environment

--- Frontend/portfolio/src/environments/environment.prod.ts
+++ Frontend/portfolio/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: '', // Set to production backend URL at deploy time
+  apiUrl: 'https://portfolio-9nl0.onrender.com', // Set to production backend URL at deploy time
   apiPrefix: '/api',
   // Set to the MongoDB ObjectId of the portfolio owner at deploy time
-  portfolioUserId: '69e73db64555bd0cc8ab3e92',
+  portfolioUserId: '6a2e8966107986bd7407efe0',
   seo: {
     siteName: 'My Portfolio',
     author: 'Polyon Mondal',
